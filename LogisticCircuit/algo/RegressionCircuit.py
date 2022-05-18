f79767b2357e9b69807f17d9e13263184684a006
David Burnett <david.b.laughs@gmail.com>
2022-05-18 03:07:25 -0400

    Extract common circuit logic to a base class
    Getting tired of copying all my changes and pasting them in two places.
    Serialization logic might be worth pulling into the abstract as well

--- LogisticCircuit/algo/RegressionCircuit.py
+++ LogisticCircuit/algo/RegressionCircuit.py
@@ -13,6 +13,8 @@ from sklearn.metrics import mean_squared_error
 
 # from .Ridge import Ridge
 from sklearn.linear_model import Ridge
+
+from .BaseCircuit import BaseCircuit
 from .BayesianRegression import BayesianRidge, ARDRegression
 from ..structure.AndGate import AndGate, AndChildNode
 from ..structure.CircuitNode import OrGate, CircuitTerminal
@@ -41,243 +43,12 @@ c
 
 
 # TODO: I feel this should extend LogisticCircuit instead of copying it, or maybe a shared base class
-class RegressionCircuit(object):
-    _vtree: Vtree
-    _num_classes: int
-    _largest_index: int
-    _num_variables: int
+class RegressionCircuit(BaseCircuit):
+    def __init__(self, vtree: Vtree, circuit_file: Optional[TextIO] = None, rand_gen: Optional[RandomState] = None):
+        super().__init__(vtree, 1, circuit_file, rand_gen)
 
-    rand_gen: RandomState
-
-    _terminal_nodes: List[Optional[CircuitTerminal]]
-    _decision_nodes: Optional[List[OrGate]]
-    _elements: Optional[List[AndGate]]
-    _parameters: Optional[torch.Tensor]
-    _covariance: Optional[List[np.ndarray]]
-    _bias: torch.Tensor
-    _root: OrGate
-
-    def __init__(self, vtree: Vtree, circuit_file: Optional[TextIO] = None,
-                 rand_gen: Optional[RandomState] = None):
-
-        self._vtree = vtree
-        self._num_classes = 1
-        self._largest_index = 0
-        self._num_variables = vtree.var_count
-
-        if rand_gen is None:
-            rand_gen = np.random.RandomState(1337)
-        self.rand_gen = rand_gen
-
-        self._terminal_nodes = [None] * 2 * self._num_variables
-        self._decision_nodes = None
-        self._elements = None
-        self._parameters = None
-        self._covariance = None
-        self._bias = torch.tensor(self.rand_gen.random_sample(size=1))
-
-        if circuit_file is None:
-            self._generate_all_terminal_nodes(vtree)
-            self._root = self._new_regression_psdd(vtree)
-        else:
-            self._root = self.load(circuit_file)
-
-        self._serialize()
-
-    @property
-    def vtree(self) -> Vtree:
-        return self._vtree
-
-    @property
-    def num_variables(self) -> int:
-        return self._num_variables
-
-    @property
-    def root(self) -> OrGate:
-        return self._root
-
-    @property
-    def num_parameters(self) -> int:
-        # numpy return self._parameters.size
-        return self._parameters.size()
-
-    @property
-    def parameters(self) -> Optional[torch.Tensor]:
-        return self._parameters
-
-    @property
-    def bias(self) -> torch.Tensor:
-        return self._bias
-
-    def _generate_all_terminal_nodes(self, vtree: Vtree) -> NoReturn:
-        if vtree.is_leaf():
-            var_index = vtree.var
-            self._terminal_nodes[var_index - 1] = CircuitTerminal(
-                self._largest_index, vtree, var_index, LITERAL_IS_TRUE,
-                torch.tensor(self.rand_gen.random_sample(size=1))
-            )
-            self._largest_index += 1
-            self._terminal_nodes[self._num_variables + var_index - 1] = CircuitTerminal(
-                self._largest_index, vtree, var_index, LITERAL_IS_FALSE,
-                torch.tensor(self.rand_gen.random_sample(size=1))
-            )
-            self._largest_index += 1
-        else:
-            self._generate_all_terminal_nodes(vtree.left)
-            self._generate_all_terminal_nodes(vtree.right)
-
-    # Same as LogisticCircuit except size=(self._num_classes,) becomes size=1
-    def _new_regression_psdd(self, vtree: Vtree) -> OrGate:
-        left_vtree: Vtree = vtree.left
-        right_vtree: Vtree = vtree.right
-        prime_variable: int = left_vtree.var
-        sub_variable: int = right_vtree.var
-        elements: List[AndGate] = list()
-        if left_vtree.is_leaf() and right_vtree.is_leaf():
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[prime_variable - 1],
-                    self._terminal_nodes[sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[prime_variable - 1],
-                    self._terminal_nodes[self._num_variables + sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[self._num_variables + prime_variable - 1],
-                    self._terminal_nodes[sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[self._num_variables + prime_variable - 1],
-                    self._terminal_nodes[self._num_variables + sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-        elif left_vtree.is_leaf():
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[prime_variable - 1],
-                    self._new_regression_psdd(right_vtree),
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements.append(
-                AndGate(
-                    self._terminal_nodes[self._num_variables + prime_variable - 1],
-                    self._new_regression_psdd(right_vtree),
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            for element in elements:
-                element.splittable_variables = copy.deepcopy(right_vtree.variables)
-        elif right_vtree.is_leaf():
-            elements.append(
-                AndGate(
-                    self._new_regression_psdd(left_vtree),
-                    self._terminal_nodes[sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements.append(
-                AndGate(
-                    self._new_regression_psdd(left_vtree),
-                    self._terminal_nodes[self._num_variables + sub_variable - 1],
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            for element in elements:
-                element.splittable_variables = copy.deepcopy(left_vtree.variables)
-        else:
-            elements.append(
-                AndGate(
-                    self._new_regression_psdd(left_vtree),
-                    self._new_regression_psdd(right_vtree),
-                    torch.tensor(self.rand_gen.random_sample(size=1))
-                )
-            )
-            elements[0].splittable_variables = copy.deepcopy(vtree.variables)
-        root = OrGate(self._largest_index, vtree, elements)
-        self._largest_index += 1
-        return root
-
-    # Same as logistic circuit
-    def _serialize(self) -> NoReturn:
-        """Serialize all the decision nodes in the logistic psdd.
-           Serialize all the elements in the logistic psdd. """
-        self._decision_nodes = [self._root]
-        self._elements = []
-        decision_node_indices = set()
-        decision_node_indices.add(self._root.index)
-        unvisited: deque[OrGate] = deque()
-        unvisited.append(self._root)
-        while len(unvisited) > 0:
-            current = unvisited.popleft()
-            for element in current.elements:
-                self._elements.append(element)
-                element.flag = False
-                if isinstance(element.prime, OrGate) and element.prime.index not in decision_node_indices:
-                    decision_node_indices.add(element.prime.index)
-                    self._decision_nodes.append(element.prime)
-                    unvisited.append(element.prime)
-                if isinstance(element.sub, OrGate) and element.sub.index not in decision_node_indices:
-                    decision_node_indices.add(element.sub.index)
-                    self._decision_nodes.append(element.sub)
-                    unvisited.append(element.sub)
-        self._parameters = self._bias.reshape(-1, 1)
-        for terminal_node in self._terminal_nodes:
-            # print(self._parameters.shape, terminal_node.parameter.reshape(-1, 1).shape)
-            self._parameters = torch.cat((self._parameters, terminal_node.parameter.reshape(-1, 1)), dim=1)
-        for element in self._elements:
-            self._parameters = torch.cat((self._parameters, element.parameter.reshape(-1, 1)), dim=1)
-        gc.collect()
-
-    # Same as LogisticCircuit except the parameter copying
-    def _record_learned_parameters(self, parameters: np.ndarray, requires_grad: bool = False) -> NoReturn:
-        # TODO: reshape required?
-        self._parameters = torch.tensor(parameters, requires_grad=requires_grad).reshape(1, -1)
-        # print("todo fix the _record_learned_parameters")
-
-        print('PARAMS', self._parameters.shape)
-        self._bias = self._parameters[:, 0]
-
-        for i in range(len(self._terminal_nodes)):
-            self._terminal_nodes[i].parameter = self._parameters[:, i + 1]
-        for i in range(len(self._elements)):
-            self._elements[i].parameter = self._parameters[:, i + 1 + 2 * self._num_variables]
-        gc.collect()
-
-    # Identical to LogisticCircuit
-    def calculate_features(self, images: np.ndarray) -> np.ndarray:
-        num_images: int = images.shape[0]
-        for terminal_node in self._terminal_nodes:
-            terminal_node.calculate_prob(images)
-        for decision_node in reversed(self._decision_nodes):
-            decision_node.calculate_prob()
-        self._root.feature = np.ones(shape=(num_images,), dtype=np.float64)
-        for decision_node in self._decision_nodes:
-            decision_node.calculate_feature()
-        # bias feature
-        bias_features = np.ones(shape=(num_images,), dtype=np.float64)
-        terminal_node_features = np.vstack(
-            [terminal_node.feature for terminal_node in self._terminal_nodes])
-        element_features = np.vstack([element.feature for element in self._elements])
-        features = np.vstack((bias_features, terminal_node_features, element_features))
-        for terminal_node in self._terminal_nodes:
-            terminal_node.feature = None
-            terminal_node.prob = None
-        for element in self._elements:
-            element.feature = None
-            element.prob = None
-        return features.T
+    def _parameter_size(self) -> Union[int, Tuple[int]]:
+        return 1
 
     # Two additional parameters over LogisticCircuit
     def _select_element_and_variable_to_split(self, data: DataSet, num_splits: int,
@@ -369,147 +140,13 @@ class RegressionCircuit(object):
         logging.info(f"{len(splits)} splits found")
         return splits
 
-    # Identical to LogisticCircuit
-    def _split(self, element_to_split: AndGate, variable_to_split: int, depth: int) -> NoReturn:
-        parent = element_to_split.parent
-        original_element, copied_element = self._copy_and_modify_element_for_split(
-            element_to_split, variable_to_split, 0, depth
-        )
-        if original_element is None or copied_element is None:
-            raise ValueError("Split elements become invalid.")
-        parent.add_element(copied_element)
-
-    # Identical to LogisticCircuit
-    def _copy_and_modify_element_for_split(self, original_element: AndGate, variable: int, current_depth: int,
-                                           max_depth: int) -> Tuple[Optional[AndGate], Optional[AndGate]]:
-        original_element.flag = True
-        original_element.remove_splittable_variable(variable)
-        original_prime = original_element.prime
-        original_sub = original_element.sub
-        copied_element: Optional[AndGate]
-        if current_depth >= max_depth:
-            if variable in original_prime.vtree.variables:
-                original_prime, copied_prime = self._copy_and_modify_node_for_split(
-                    original_prime, variable, current_depth, max_depth
-                )
-                copied_sub = original_sub
-            elif variable in original_sub.vtree.variables:
-                original_sub, copied_sub = self._copy_and_modify_node_for_split(
-                    original_sub, variable, current_depth, max_depth
-                )
-                copied_prime = original_prime
-            else:
-                copied_prime = original_prime
-                copied_sub = original_sub
-        else:
-            original_prime, copied_prime = self._copy_and_modify_node_for_split(
-                original_prime, variable, current_depth, max_depth
-            )
-            original_sub, copied_sub = self._copy_and_modify_node_for_split(
-                original_sub, variable, current_depth, max_depth)
-        if copied_prime is not None and copied_sub is not None:
-            copied_element = AndGate(copied_prime, copied_sub,
-                                     copy.deepcopy(original_element.parameter))
-            copied_element.splittable_variables = copy.deepcopy(
-                original_element.splittable_variables)
-        else:
-            copied_element = None
-        if original_prime is not None and original_sub is not None:
-            original_element.prime = original_prime
-            original_element.sub = original_sub
-        else:
-            original_element = None
-        return original_element, copied_element
-
-    # Identical to LogisticCircuit
-    def _copy_and_modify_node_for_split(self, original_node: AndChildNode, variable: int, current_depth: int,
-                                        max_depth: int) -> Tuple[Optional[AndChildNode], Optional[AndChildNode]]:
-        if original_node.num_parents == 0:
-            raise ValueError("Some node does not have a parent.")
-        original_node.decrease_num_parents_by_one()
-        copied_node: Optional[AndChildNode]
-        if isinstance(original_node, CircuitTerminal):
-            if original_node.var_index == variable:
-                if original_node.var_value == LITERAL_IS_TRUE:
-                    copied_node = None
-                elif original_node.var_value == LITERAL_IS_FALSE:
-                    original_node = None
-                    copied_node = self._terminal_nodes[self._num_variables + variable - 1]
-                else:
-                    raise ValueError(
-                        "Under the current setting,"
-                        "we only support terminal nodes that are either positive or negative literals."
-                    )
-            else:
-                copied_node = original_node
-            return original_node, copied_node
-        else:
-            if original_node.num_parents > 0:
-                original_node = self._deep_copy_node(
-                    original_node, variable, current_depth, max_depth)
-            copied_elements = []
-            i = 0
-            while i < len(original_node.elements):
-                original_element, copied_element = self._copy_and_modify_element_for_split(
-                    original_node.elements[i], variable, current_depth + 1, max_depth
-                )
-                if original_element is None:
-                    original_node.remove_element(i)
-                else:
-                    i += 1
-                if copied_element is not None:
-                    copied_elements.append(copied_element)
-            if len(copied_elements) == 0:
-                copied_node = None
-            else:
-                self._largest_index += 1
-                copied_node = OrGate(self._largest_index, original_node.vtree, copied_elements)
-            if len(original_node.elements) == 0:
-                original_node = None
-            return original_node, copied_node
-
-    # Identical to LogisticCircuit
-    def _deep_copy_node(self, node: AndChildNode, variable: int, current_depth: int, max_depth: int) -> AndChildNode:
-        if isinstance(node, CircuitTerminal):
-            return node
-        else:
-            if len(node.elements) == 0:
-                raise ValueError("Decision nodes should have at least one elements.")
-            copied_elements = []
-            for element in node.elements:
-                copied_elements.append(self._deep_copy_element(
-                    element, variable, current_depth + 1, max_depth))
-            self._largest_index += 1
-            return OrGate(self._largest_index, node.vtree, copied_elements)
-
-    # Identical to LogisticCircuit
-    def _deep_copy_element(self, element: AndGate, variable: int, current_depth: int, max_depth: int) -> AndGate:
-        copied_element: AndGate
-        if current_depth >= max_depth:
-            if variable in element.prime.vtree.variables:
-                copied_element = AndGate(
-                    self._deep_copy_node(element.prime, variable, current_depth, max_depth),
-                    element.sub,
-                    element.parameter.clone(),  # copy.deepcopy(element.parameter),
-                )
-            elif variable in element.sub.vtree.variables:
-                copied_element = AndGate(
-                    element.prime,
-                    self._deep_copy_node(element.sub, variable, current_depth, max_depth),
-                    element.parameter.clone(),  # copy.deepcopy(element.parameter),
-                )
-            else:
-                copied_element = AndGate(element.prime, element.sub,
-                                         element.parameter.clone(),  # copy.deepcopy(element.parameter)
-                                         )
-        else:
-            copied_element = AndGate(
-                self._deep_copy_node(element.prime, variable, current_depth, max_depth),
-                self._deep_copy_node(element.sub, variable, current_depth, max_depth),
-                element.parameter.clone(),  # copy.deepcopy(element.parameter),
-            )
-        copied_element.splittable_variables = copy.deepcopy(element.splittable_variables)
-        return copied_element
+    # Same as LogisticCircuit except the parameter copying
+    def _record_learned_parameters(self, parameters: np.ndarray, requires_grad: bool = False) -> NoReturn:
+        # TODO: reshape required?
+        self._parameters = torch.tensor(parameters, requires_grad=requires_grad).reshape(1, -1)
+        # print("todo fix the _record_learned_parameters")
+        # print('PARAMS', self._parameters.shape)
+        self._set_node_parameters(self._parameters)
 
     # def calculate_accuracy(self, data):
     #     """Calculate accuracy given the learned parameters on the provided data."""
@@ -709,7 +346,7 @@ class RegressionCircuit(object):
             raise ValueError("Circuit must have at least one decision node to represent the root node")
 
         if line[0] != "B":
-            raise ValueError("The last line in a circuit file must record the bias parameters.")
+            raise ValueError("After decision nodes in a circuit must record the bias parameters.")
         self._bias = torch.tensor([float(x) for x in line.strip().split(" ")[1:]], dtype=torch.float64)
 
         # the parameters vector will be reconstructed after loading, but the covariance will need to be read
