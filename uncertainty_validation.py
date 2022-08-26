3c36a083321a9ea1898f8b3a7c2b60805a0bf2eb
David Burnett <david.b.laughs@gmail.com>
2022-08-26 19:16:36 -0400

    Cleanup: move common logic for summarizing experiments to a single function
    Summarizing is now always batched which should lead to a small speedup on delta method experiments.
    In addition, the parallel over samples function now has a variable number of returns which should let us further boost the speed for the delta method by batching the second moment calculations

--- uncertainty_validation.py
+++ uncertainty_validation.py
@@ -1,7 +1,7 @@
 import copy
 import numpy as np
 import torch
-from typing import Tuple
+from typing import Tuple, List, Optional
 
 from torch import Tensor
 
@@ -17,14 +17,17 @@ from sklearn.linear_model._logistic import (_joblib_parallel_args)
 from joblib import Parallel, delayed
 
 
+SummaryType = Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]
+
+
 def _gaussianLogLikelihood(x: torch.Tensor, mean: torch.Tensor, var: torch.Tensor) -> torch.Tensor:
     """Evaluates the log likelihood for a gaussian distribution"""
     return -0.5 * torch.log(torch.mul(2 * torch.pi, var))\
         - 0.5 / var * ((x - mean) ** 2)
 
 
-def _baseGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, jobs: int, function, *args
-                               ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+def _baseParallelOverSamples(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, jobs: int, function, *args
+                             ) -> List[Tensor]:
     """
     Shared logic between any method that parallels over count
     @param psdd:      Probabilistic circuit root
@@ -33,8 +36,7 @@ def _baseGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSe
     @param jobs:      Max number of parallel jobs to run, use -1 to use the max possible
     @param function:  Parallel function to run, handles the actual LL computation
     @param args:      Extra arguments to pass into the function
-    @return  Tuple of total error, average input LL, average param LL, average total LL,
-             average input variance, average param variance, average total variance
+    @return  Tuple of function returns
     """
     count = dataset.labels.size()[0]
 
@@ -45,20 +47,39 @@ def _baseGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSe
         delayedFunc(psdd, lgc, dataset.images[i, :], dataset.labels[i], i, *args)
         for i in range(count)
     )
-    error, \
-        inputLikelihood, parameterLikelihood, totalLikelihood, \
-        inputVariances, parameterVariances, totalVariances = zip(*result)
+    resultTensors: List[Optional[torch.Tensor]] = [None]*len(result[0])
+    for i, result in enumerate(zip(*result)):
+        resultTensors[i] = torch.tensor(result)
     gc.collect()
+    return resultTensors
 
-    # return all six values
-    return torch.sum(torch.tensor(error)), torch.mean(torch.tensor(inputLikelihood)),\
-        torch.mean(torch.tensor(parameterLikelihood)), torch.mean(torch.tensor(totalLikelihood)), \
-        torch.mean(torch.tensor(inputVariances)), torch.mean(torch.tensor(parameterVariances)),\
-        torch.mean(torch.tensor(totalVariances))
+
+def _summarize(dataset: DataSet, mean: torch.Tensor,
+               inputVariances: torch.Tensor, parameterVariances: torch.Tensor, totalVariances: torch.Tensor
+               ) -> SummaryType:
+    """
+    Helper to summarize the results of an experiment
+
+    @param dataset             The dataset used for the experiment
+    @param mean                Tensor of averages with a value per sample in the dataset
+    @param inputVariances      Tensor of input variances with a value per sample in the dataset
+    @param parameterVariances  Tensor of parameter variances with a value per sample in the dataset
+    @param totalVariances      Tensor of total variances with a value per sample in the dataset
+    @return  Total error, average loglikelihood for each variance, average variance
+    """
+    error = torch.abs(mean - dataset.labels)
+    inputLikelihood = _gaussianLogLikelihood(dataset.labels, mean, inputVariances)
+    parameterLikelihood = _gaussianLogLikelihood(dataset.labels, mean, parameterVariances)
+    totalLikelihood = _gaussianLogLikelihood(dataset.labels, mean, totalVariances)
+
+    return torch.sum(error), torch.mean(inputLikelihood),\
+        torch.mean(parameterLikelihood), torch.mean(totalLikelihood), \
+        torch.mean(inputVariances), torch.mean(parameterVariances),\
+        torch.mean(totalVariances)
 
 
 def _monteCarloIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.ndarray,
-                         y: torch.Tensor, i: int, params: MonteCarloParams) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                         y: torch.Tensor, i: int, params: MonteCarloParams) -> Tuple[Tensor, Tensor, Tensor]:
     """Evaluates a single iteration of the monte carlo method, used for parallel"""
     # evaluate model
     feature = feature.reshape(1, -1)
@@ -66,17 +87,11 @@ def _monteCarloIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.ndarray,
     lgc = copy.deepcopy(lgc)
     mean, sampleParamVar, sampleInputVar = monteCarloPrediction(psdd, lgc, params, feature, prefix=f"var {i}")
     # add variances directly
-    totalVariance = sampleInputVar + sampleParamVar
-    # compute likelihoods
-    error = torch.abs(mean - y)
-    inputLikelihood = _gaussianLogLikelihood(y, mean, sampleInputVar)
-    parameterLikelihood = _gaussianLogLikelihood(y, mean, sampleParamVar)
-    totalLikelihood = _gaussianLogLikelihood(y, mean, totalVariance)
-    return error, inputLikelihood, parameterLikelihood, totalLikelihood, sampleInputVar, sampleParamVar, totalVariance
+    return mean, sampleInputVar, sampleParamVar
 
 
 def monteCarloGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, params: MonteCarloParams,
-                                    jobs: int = -1) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                                    jobs: int = -1) -> SummaryType:
     """
     Computes likelihood and variances over the entire dataset
     @param psdd:      Probabilistic circuit root
@@ -87,11 +102,15 @@ def monteCarloGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: D
     @return  Tuple of total error, average input LL, average param LL, average total LL,
              average input variance, average param variance, average total variance
     """
-    return _baseGaussianLogLikelihood(psdd, lgc, dataset, jobs, _monteCarloIteration, params)
+    mean, inputVariances, parameterVariances = _baseParallelOverSamples(psdd, lgc, dataset, jobs, _monteCarloIteration, params)
+    totalVariances = parameterVariances + inputVariances
+
+    gc.collect()
+    return _summarize(dataset, mean, inputVariances, parameterVariances, totalVariances)
 
 
 def fastMonteCarloGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, params: MonteCarloParams,
-                                        jobs: int = -1) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                                        jobs: int = -1) -> SummaryType:
     """
     Computes likelihood and variances over the entire dataset,
     performing parallel over parameters and using torch to do parallel over inputs
@@ -105,24 +124,14 @@ def fastMonteCarloGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, datase
     """
     # prepare parallel
     mean, parameterVariances, inputVariances = monteCarloPredictionParallel(psdd, lgc, params, dataset.images, jobs=jobs)
-
     totalVariances = parameterVariances + inputVariances
-    error = torch.abs(mean - dataset.labels)
-    inputLikelihood = _gaussianLogLikelihood(dataset.labels, mean, inputVariances)
-    parameterLikelihood = _gaussianLogLikelihood(dataset.labels, mean, parameterVariances)
-    totalLikelihood = _gaussianLogLikelihood(dataset.labels, mean, totalVariances)
 
     gc.collect()
-
-    # return all six values
-    return torch.sum(error), torch.mean(inputLikelihood),\
-        torch.mean(parameterLikelihood), torch.mean(totalLikelihood), \
-        torch.mean(inputVariances), torch.mean(parameterVariances),\
-        torch.mean(totalVariances)
+    return _summarize(dataset, mean, inputVariances, parameterVariances, totalVariances)
 
 
 def _deltaGaussianIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.ndarray, y: torch.Tensor, i: int
-                            ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                            ) -> Tuple[Tensor, Tensor, Tensor]:
     """Evaluates a single iteration of the delta method, used for parallel"""
 
     print(f"Evaluating delta method {i}", end='\r')
@@ -140,18 +149,11 @@ def _deltaGaussianIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.ndarra
     sampleInputVar = deltaInputVariance(psdd, lgc, cache, feature)
     # add variances directly
     inputVariance = torch.clamp(sampleInputVar, min=0)  # was abs before
-    # TODO validated why it goes negative
-    totalVariance = inputVariance + sampleParamVar
-    # compute likelihoods
-    error = torch.abs(mean - y)
-    inputLikelihood = _gaussianLogLikelihood(y, mean, inputVariance)
-    parameterLikelihood = _gaussianLogLikelihood(y, mean, sampleParamVar)
-    totalLikelihood = _gaussianLogLikelihood(y, mean, totalVariance)
-    return error, inputLikelihood, parameterLikelihood, totalLikelihood, inputVariance, sampleParamVar, totalVariance
+    return mean, inputVariance, sampleParamVar
 
 
 def deltaGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, jobs: int = -1
-                               ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                               ) -> SummaryType:
     """
     Computes likelihood and variances over the entire dataset
     @param psdd:    Probabilistic circuit root
@@ -161,11 +163,15 @@ def deltaGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSe
     @return  Tuple of total error, average input LL, average param LL, average total LL,
              average input variance, average param variance, average total variance
     """
-    return _baseGaussianLogLikelihood(psdd, lgc, dataset, jobs, _deltaGaussianIteration)
+    mean, inputVariance, paramVariance = _baseParallelOverSamples(psdd, lgc, dataset, jobs, _deltaGaussianIteration)
+    totalVariance = inputVariance + paramVariance
+
+    gc.collect()
+    return _summarize(dataset, mean, inputVariance, paramVariance, totalVariance)
 
 
 def _exactDeltaGaussianIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.ndarray, y: torch.Tensor, i: int
-                                 ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                                 ) -> Tuple[Tensor, Tensor, Tensor]:
     """Evaluates a single iteration of the exact delta method, used for parallel"""
 
     print(f"Evaluating delta method {i}", end='\r')
@@ -180,20 +186,11 @@ def _exactDeltaGaussianIteration(psdd: PSddNode, lgc: BaseCircuit, feature: np.n
     # TODO: why do I not pass in the mean?
     cache = EVCache()
     totalVariance = exactDeltaTotalVariance(psdd, lgc, cache, feature)
-    # add variances directly
-    # inputVariance = torch.clamp(sampleInputVar, min=0)  # was abs before
-    # TODO validated why it goes negative
-    inputVariance = totalVariance - sampleParamVar
-    # compute likelihoods
-    error = torch.abs(mean - y)
-    inputLikelihood = _gaussianLogLikelihood(y, mean, inputVariance)
-    parameterLikelihood = _gaussianLogLikelihood(y, mean, sampleParamVar)
-    totalLikelihood = _gaussianLogLikelihood(y, mean, totalVariance)
-    return error, inputLikelihood, parameterLikelihood, totalLikelihood, inputVariance, sampleParamVar, totalVariance
+    return mean, sampleParamVar, totalVariance
 
 
 def exactDeltaGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: DataSet, jobs: int = -1
-                                    ) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, Tensor]:
+                                    ) -> SummaryType:
     """
     Computes likelihood and variances over the entire dataset
     @param psdd:    Probabilistic circuit root
@@ -203,4 +200,8 @@ def exactDeltaGaussianLogLikelihood(psdd: PSddNode, lgc: BaseCircuit, dataset: D
     @return  Tuple of total error, average input LL, average param LL, average total LL,
              average input variance, average param variance, average total variance
     """
-    return _baseGaussianLogLikelihood(psdd, lgc, dataset, jobs, _exactDeltaGaussianIteration)
+    mean, paramVariance, totalVariance = _baseParallelOverSamples(psdd, lgc, dataset, jobs, _exactDeltaGaussianIteration)
+    inputVariance = totalVariance - paramVariance
+
+    gc.collect()
+    return _summarize(dataset, mean, inputVariance, paramVariance, totalVariance)
