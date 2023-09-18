Fix issue with conditional gaussian computation
Turns out the mathematically compact and in theory more efficient way to compute the conditional covariance matrix is less computationally stable, so most people just invert a slice of the inverted covariance matrix.
For efficiency, compute the inverted covariance matrix just once

scripts/evaluate_dataset_uncertainty.py
@@ -157,9 +157,6 @@ if __name__ == '__main__':
 
     parser.add_argument("--benchmark_time",  action='store_true',
                         help="If set, disables batching on several methods to make the times more comparable")
-    parser.add_argument("--include_conditional",  action='store_true',
-                        help="If set, includes the conditional gaussian method when relevant. Can potentially lead to "
-                             "a crash with the inverse not converging that is still being debugged.")
 
     # Experiment configuration
     parser.add_argument("--missing", type=float, nargs='*', help="Percent of data to treat as missing")
@@ -436,8 +433,10 @@ if __name__ == '__main__':
                 trainingSampleCov = np.cov(trainingImages, rowvar=False)
             else:
                 trainingSampleCov = np.cov(trainingData.images, rowvar=False)
+            # needed for conditional covariance matrixes, might as well compute just once
+            trainingSampleCovInv = np.linalg.pinv(trainingSampleCov)
 
-        # second loop is over missing value counts
+        # delta method using gradients for parameter uncertainty and moment for input uncertainty
         if not args.skip_delta:
             method = deltaGaussianLogLikelihoodBenchmarkTime if args.benchmark_time else deltaGaussianLogLikelihood
             run_experiment("Moment + Delta", percent, method, psdd, lgc, zero_grad=True)
@@ -460,40 +459,43 @@ if __name__ == '__main__':
                 return conditionalMeanImputation(inputs, trainingSampleMean, trainingSampleCov,
                                                  enforceBoolean=enforceBoolean)
 
+            # Basic imputation: runs the regressor handling missing values via imputation
+            # Expectation is Expected Predictions first moment
             if args.include_trivial:
                 run_experiment("Mean Imputation", percent, basicImputation, basicMeanImputation, lgc)
-                if args.include_conditional:
-                    run_experiment("Conditional Imputation", percent, basicImputation, basicConditionalImputation, lgc)
+                run_experiment("Conditional Imputation", percent, basicImputation, basicConditionalImputation, lgc)
                 run_experiment("Expectation", percent, basicExpectation, psdd, lgc)
             if args.include_residual_input:
                 run_experiment("Imputation + Residual", percent,
                                residualPerSampleInput, pureValidSet,
                                basicImputation, basicMeanImputation, lgc)
-                if args.include_conditional:
-                    run_experiment("Conditional + Residual", percent,
-                                   residualPerSampleInput, pureValidSet,
-                                   basicImputation, basicConditionalImputation, lgc)
+                run_experiment("Conditional + Residual", percent,
+                               residualPerSampleInput, pureValidSet,
+                               basicImputation, basicConditionalImputation, lgc)
                 run_experiment("Expectation + Residual", percent,
                                residualPerSampleInput, pureValidSet,
                                basicExpectation, psdd, lgc)
 
+        # Handles input uncertainty simply as the second moment
         if args.input_baseline:
             method = inputLogLikelihoodBenchmarkTime if args.benchmark_time else inputLogLikelihood
             run_experiment("Moment only", percent, method, psdd, lgc)
+
+        # Handle input uncertainty as samples from a monte carlo gaussian
         if args.input_samples > 1:
-            if args.include_conditional:
-                run_experiment("Conditional MC {} only".format(args.input_samples), percent,
-                               monteCarloGaussianInputOnlyLogLikelihood, lgc,
-                               trainingSampleMean, trainingSampleCov, args.input_samples,
-                               conditionalGaussian, randState, enforceBoolean)
+            run_experiment("Conditional MC {} only".format(args.input_samples), percent,
+                           monteCarloGaussianInputOnlyLogLikelihood, lgc,
+                           trainingSampleMean, trainingSampleCov, args.input_samples, trainingSampleCovInv,
+                           conditionalGaussian, randState, enforceBoolean)
             run_experiment("Marginalize MC {} only".format(args.input_samples), percent,
                            monteCarloGaussianInputOnlyLogLikelihood, lgc,
-                           trainingSampleMean, trainingSampleCov, args.input_samples,
+                           trainingSampleMean, trainingSampleCov, args.input_samples, trainingSampleCovInv,
                            marginalizeGaussian, randState, enforceBoolean)
         if args.psdd_samples > 1:
             run_experiment("PSDD MC {} only".format(args.psdd_samples), percent,
                            monteCarloPSDDInputOnlyLogLikelihood, psdd, lgc, args.psdd_samples, randState)
 
+        # Monte carlo over parameter uncertainty
         # Fast monte carlo, lets me get the accuracy far closer to Delta with less of a runtime hit
         if args.samples > 1:
             params = sampleMonteCarloParameters(lgc, args.samples, randState)
@@ -503,20 +505,20 @@ if __name__ == '__main__':
             if args.parameter_baseline:
                 # Alternative parameter baseline, just ignores input uncertainty without mean imputation
                 run_experiment("Expectation + MC {}".format(args.samples), percent, method, psdd, lgc, params, True)
+                # TODO: consider conditional variant here
                 # Standard baseline with mean imputation
                 run_experiment("Imputation + MC {}".format(args.samples), percent,
                                monteCarloParamLogLikelihood, trainingSampleMean, lgc, params)
 
             if args.input_samples > 1:
-                if args.include_conditional:
-                    run_experiment("Conditional MC {} + MC {}".format(args.input_samples, args.samples), percent,
-                                   monteCarloGaussianParamInputLogLikelihood, lgc, params,
-                                   trainingSampleMean, trainingSampleCov, args.input_samples,
-                                   conditionalGaussian, randState, enforceBoolean
-                    )
+                run_experiment("Conditional MC {} + MC {}".format(args.input_samples, args.samples), percent,
+                               monteCarloGaussianParamInputLogLikelihood, lgc, params,
+                               trainingSampleMean, trainingSampleCov, args.input_samples, trainingSampleCovInv,
+                               conditionalGaussian, randState, enforceBoolean
+                )
                 run_experiment("Marginalize MC {} + MC {}".format(args.input_samples, args.samples), percent,
                                monteCarloGaussianParamInputLogLikelihood, lgc, params,
-                               trainingSampleMean, trainingSampleCov, args.input_samples,
+                               trainingSampleMean, trainingSampleCov, args.input_samples, trainingSampleCovInv,
                                marginalizeGaussian, randState, enforceBoolean
                 )
             if args.psdd_samples > 1:

uncertainty_calculations.py
@@ -263,7 +263,7 @@ def exactDeltaTotalVariance(psdd: PSddNode, lgc: BaseCircuit, cache: EVCache, ob
 
 def _monteCarloGaussianInputIteration(lgc: BaseCircuit, param: Optional[torch.Tensor],
                                       inputMean: np.ndarray, inputCovariance: np.ndarray, inputSamples: int,
-                                      inputReducer: callable, obsX: np.ndarray = None,
+                                      inputCovarianceInv: np.ndarray, inputReducer: callable, obsX: np.ndarray = None,
                                       prefix: str = '', i: int = -1, seed: int = 1337, randState: RandomState = None,
                                       enforceBoolean: bool = True
                                       ) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -276,7 +276,7 @@ def _monteCarloGaussianInputIteration(lgc: BaseCircuit, param: Optional[torch.Te
     # the goal here is to process all test samples and all input samples in one large batch of size test * input
     # start by constructing a 3D matrix of test sample * input sample * feature
     obsXAugmented = augmentMonteCarloSamplesGaussian(
-        inputMean, inputCovariance, inputSamples, inputReducer, obsX, seed, randState,
+        inputMean, inputCovariance, inputSamples, inputCovarianceInv, inputReducer, obsX, seed, randState,
         enforceBoolean=enforceBoolean
     )
 
@@ -302,7 +302,7 @@ def _monteCarloGaussianInputIteration(lgc: BaseCircuit, param: Optional[torch.Te
 
 
 def monteCarloGaussianInputOnly(lgc: BaseCircuit, inputMean: np.ndarray, inputCovariance: np.ndarray, inputSamples: int,
-                                inputReducer: callable = conditionalGaussian,
+                                inputCovarianceInv: np.ndarray = None, inputReducer: callable = conditionalGaussian,
                                 randState: RandomState = None, obsX: np.ndarray = None, enforceBoolean: bool = True
                                 ) -> Tuple[torch.Tensor, torch.Tensor]:
     """
@@ -311,6 +311,7 @@ def monteCarloGaussianInputOnly(lgc: BaseCircuit, inputMean: np.ndarray, inputCo
     @param lgc:              Logistic or regression circuit
     @param inputMean:        Mean vector of the input distribution
     @param inputCovariance:  Covariance matrix of the input distribution
+    @param inputCovarianceInv:  Inverted covariance matrix of the input distribution, if None will compute as needed
     @param inputSamples:     Number of samples to take from the input distribution
     @param inputReducer:     Function to reduce the random variables, typically marginal or conditional
     @param randState:        Random state for sampling inputs
@@ -324,7 +325,7 @@ def monteCarloGaussianInputOnly(lgc: BaseCircuit, inputMean: np.ndarray, inputCo
         randState = RandomState(1337)
 
     means, covariances = _monteCarloGaussianInputIteration(
-        lgc, None, inputMean, inputCovariance, inputSamples, inputReducer, obsX,
+        lgc, None, inputMean, inputCovariance, inputSamples, inputCovarianceInv, inputReducer, obsX,
         randState=randState, enforceBoolean=enforceBoolean
     )
 
@@ -334,7 +335,7 @@ def monteCarloGaussianInputOnly(lgc: BaseCircuit, inputMean: np.ndarray, inputCo
 
 def monteCarloGaussianParamAndInput(lgc: BaseCircuit, params: MonteCarloParams,
                                     inputMean: np.ndarray, inputCovariance: np.ndarray, inputSamples: int,
-                                    inputReducer: callable = conditionalGaussian,
+                                    inputCovarianceInv: np.ndarray = None, inputReducer: callable = conditionalGaussian,
                                     randState: RandomState = None, obsX: np.ndarray = None,
                                     prefix: str = '', jobs: int = -1, enforceBoolean: bool = True
                                     ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
@@ -345,6 +346,7 @@ def monteCarloGaussianParamAndInput(lgc: BaseCircuit, params: MonteCarloParams,
     @param params:           List of parameters to use for the samples
     @param inputMean:        Mean vector of the input distribution
     @param inputCovariance:  Covariance matrix of the input distribution
+    @param inputCovarianceInv:  Inverted covariance matrix of the input distribution, if None will compute as needed
     @param inputSamples:     Number of samples to take from the input distribution
     @param inputReducer:     Function to reduce the random variables, typically marginal or conditional
     @param randState:        Random state for sampling inputs
@@ -362,7 +364,7 @@ def monteCarloGaussianParamAndInput(lgc: BaseCircuit, params: MonteCarloParams,
     delayedFunc = delayed(_monteCarloGaussianInputIteration)
     result = Parallel(n_jobs=jobs,
                       **_joblib_parallel_args(prefer='processes'))(
-        delayedFunc(lgc, param, inputMean, inputCovariance, inputSamples, inputReducer, obsX,
+        delayedFunc(lgc, param, inputMean, inputCovariance, inputSamples, inputCovarianceInv, inputReducer, obsX,
                     prefix, i, randState.randint(0, 2**32 - 1), None, enforceBoolean)
         for i, param in enumerate(params)
     )

uncertainty_utils.py
@@ -72,7 +72,10 @@ def parallelOverSamples(dataset: DataSet, jobs: int, function, *args) -> List[Te
     return resultTensors
 
 
-def marginalizeGaussian(inputs: np.ndarray, mean: np.ndarray, covariance: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+# noinspection PyUnusedLocal
+# Extra parameter used for parity with conditional gaussian
+def marginalizeGaussian(inputs: np.ndarray, mean: np.ndarray, covariance: np.ndarray, covarianceInv: np.ndarray = None
+                        ) -> Tuple[np.ndarray, np.ndarray]:
     """
     Produces a marginal gaussian distribution over all the missing variables in the input vector
     """
@@ -80,7 +83,8 @@ def marginalizeGaussian(inputs: np.ndarray, mean: np.ndarray, covariance: np.nda
     return mean[missingIndexes], covariance[np.ix_(missingIndexes, missingIndexes)]
 
 
-def conditionalGaussian(inputs: np.array, mean: np.ndarray, covariance: np.ndarray, returnCovariance: bool = True
+def conditionalGaussian(inputs: np.array, mean: np.ndarray, covariance: np.ndarray, covarianceInv: np.ndarray = None,
+                        returnCovariance: bool = True
                         ) -> Union[Optional[np.ndarray], Tuple[Optional[np.ndarray], Optional[np.ndarray]]]:
     """
     Produces a conditional gaussian distribution over all the missing variables in the input vector given observed
@@ -112,8 +116,16 @@ def conditionalGaussian(inputs: np.array, mean: np.ndarray, covariance: np.ndarr
         return condMean
 
     # Final computed conditional variance
-    condVar = covariance[np.ix_(missingIndexes, missingIndexes)] \
-        - np.matmul(np.matmul(corrMatrix, obsCovInv), corrMatrix.T)
+    # The following formula is more efficient (no need to compute an additional inverse)
+    # but leads to a crash on some datasets as it produces a matrix that is not positive semi-definiate
+    # condVar = covariance[np.ix_(missingIndexes, missingIndexes)] \
+    #     - np.matmul(np.matmul(corrMatrix, obsCovInv), corrMatrix.T)
+
+    # this formula is mathematically equivalent, but is less efficient
+    # however, we should be guaranteed a valid covariance matrix at the end
+    if covarianceInv is None:
+        covarianceInv = np.linalg.pinv(covariance)
+    condVar = np.linalg.pinv(covarianceInv[np.ix_(missingIndexes, missingIndexes)])
     return condMean, condVar
 
 

uncertainty_validation.py
@@ -556,6 +556,7 @@ def residualPerSampleInput(validationData: DataSet, experiment_function, *experi
 
 def monteCarloGaussianInputOnlyLogLikelihood(lgc: BaseCircuit,
                                              inputMean: np.ndarray, inputCovariance: np.ndarray, inputSamples: int,
+                                             inputCovarianceInv: np.ndarray = None,
                                              inputReducer: callable = conditionalGaussian,
                                              randState: RandomState = None, enforceBoolean: bool = True,
                                              dataset: DataSet = None, summaryFunction: SummaryFunction = None,
@@ -568,6 +569,7 @@ def monteCarloGaussianInputOnlyLogLikelihood(lgc: BaseCircuit,
     @param dataset:             Dataset for computing the full value
     @param inputMean:           Mean vector of the input distribution
     @param inputCovariance:     Covariance matrix of the input distribution
+    @param inputCovarianceInv:  Inverted covariance matrix of the input distribution, if None will compute as needed
     @param inputSamples:        Number of samples to take from the input distribution
     @param inputReducer:        Function to reduce the random variables, typically marginal or conditional
     @param randState:           Random state for sampling inputs
@@ -579,7 +581,7 @@ def monteCarloGaussianInputOnlyLogLikelihood(lgc: BaseCircuit,
              average input variance, average param variance, average total variance
     """
     mean, inputVariances = monteCarloGaussianInputOnly(
-        lgc, inputMean, inputCovariance, inputSamples, inputReducer, randState, dataset.images,
+        lgc, inputMean, inputCovariance, inputSamples, inputCovarianceInv, inputReducer, randState, dataset.images,
         enforceBoolean=enforceBoolean
     )
     totalVariances = inputVariances + residualUncertainty
@@ -592,6 +594,7 @@ def monteCarloGaussianInputOnlyLogLikelihood(lgc: BaseCircuit,
 
 def monteCarloGaussianParamInputLogLikelihood(lgc: BaseCircuit, params: MonteCarloParams,
                                               inputMean: np.ndarray, inputCovariance: np.ndarray, inputSamples: int,
+                                              inputCovarianceInv: np.ndarray = None,
                                               inputReducer: callable = conditionalGaussian,
                                               randState: RandomState = None, enforceBoolean: bool = True,
                                               dataset: DataSet = None, jobs: int = -1,
@@ -605,6 +608,7 @@ def monteCarloGaussianParamInputLogLikelihood(lgc: BaseCircuit, params: MonteCar
     @param params:              Parameters to use for estimates
     @param inputMean:           Mean vector of the input distribution
     @param inputCovariance:     Covariance matrix of the input distribution
+    @param inputCovarianceInv:  Inverted covariance matrix of the input distribution, if None will compute as needed
     @param inputSamples:        Number of samples to take from the input distribution
     @param inputReducer:        Function to reduce the random variables, typically marginal or conditional
     @param randState:           Random state for sampling inputs
@@ -616,7 +620,7 @@ def monteCarloGaussianParamInputLogLikelihood(lgc: BaseCircuit, params: MonteCar
              average input variance, average param variance, average total variance
     """
     mean, parameterVariances, inputVariances = monteCarloGaussianParamAndInput(
-        lgc, params, inputMean, inputCovariance, inputSamples, inputReducer, randState,
+        lgc, params, inputMean, inputCovariance, inputSamples, inputCovarianceInv, inputReducer, randState,
         dataset.images, jobs=jobs, prefix="Input Gaussian", enforceBoolean=enforceBoolean
     )
     totalVariances = parameterVariances + inputVariances + residualUncertainty